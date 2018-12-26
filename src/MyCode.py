3bc71f2c5f5026cdff2ba47fe0bc559eb4bd57e8
Akshat Gupta <rockstarakshat7@gmail.com>
2018-12-26 14:36:28 +0530

    code

---
+++ src/MyCode.py
@@ -0,0 +1,14 @@
+import random
+var = ["A", "B", "C", "D", "E", "F", "G", "H", "I", 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '&', '*', '(', ')']
+
+quan=int(input("How many passwords would you like to generate? :-"))
+
+length=int(input("What should be the password length? :- "))
+
+print("Here Are Your Passwords :")
+while quan:
+    password=''
+    for i in range(length):
+        password+=random.choice(var)
+    print(password)
+    quan=quan-1
